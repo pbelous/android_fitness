271d4a41f856e557964dbec113ede1ab20d0abaf
pbelous <belp1@yandex.ru>
2015-10-02 11:58:47 +0300

    1

---
+++ app/src/main/java/com/fitness/fitness/WeightStatsActivity.java
@@ -0,0 +1,28 @@
+package com.fitness.fitness;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+
+public class WeightStatsActivity extends AppCompatActivity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weight_stat);
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
+    }
+
+}
