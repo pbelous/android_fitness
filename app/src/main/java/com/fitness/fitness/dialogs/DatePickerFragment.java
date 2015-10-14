b1d81d9ec80fb688d11a81f7054165182003bc50
pbelous <belp1@yandex.ru>
2015-10-14 15:25:25 +0300

    1

--- app/src/main/java/com/fitness/fitness/dialogs/DatePickerFragment.java
+++ app/src/main/java/com/fitness/fitness/dialogs/DatePickerFragment.java
@@ -22,7 +22,6 @@ public class DatePickerFragment extends DialogFragment
         int month = c.get(Calendar.MONTH);
         int day = c.get(Calendar.DAY_OF_MONTH);
 
-        // Create a new instance of DatePickerDialog and return it
         return new DatePickerDialog(getActivity(), this, year, month, day);
     }
 
